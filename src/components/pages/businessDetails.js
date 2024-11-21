55eadf60052bc2c25f5d983cc00d3949625369fd
Karthikk7293 <karthikk7293@gmail.com>
2024-11-21 17:52:02 +0530

    📦 NEW: added youtube and linkedin in social media sections

--- src/components/pages/businessDetails.js
+++ src/components/pages/businessDetails.js
@@ -96,9 +96,9 @@ const BusinessDetails = () => {
 
         setSocialLinks(businessData?.data?.socialMediaLinks);
         setCategory(businessData?.data.category);
-        setLoading(false)
+        setLoading(false);
       } catch (error) {
-        setLoading(false)
+        setLoading(false);
         console.error(
           "Error fetching business details:",
           error.message || error
@@ -141,7 +141,9 @@ const BusinessDetails = () => {
     if (
       e.target.name === "facebook" ||
       e.target.name === "twitter" ||
-      e.target.name === "instagram"
+      e.target.name === "instagram" ||
+      e.target.name === "youtube" ||
+      e.target.name === "linkedIn"
     ) {
       const array = [...formData.socialMediaLinks];
       function updateLinkById(array, tag, newLink) {
@@ -238,13 +240,13 @@ const BusinessDetails = () => {
   return (
     <>
       <div className="m-4 mx-auto bg-transparent shadow-md rounded-lg p-6 mt-4 border border-gray-200 relative">
-        {imageLoading || loading && <FullPageLoader />}
+        {imageLoading || (loading && <FullPageLoader />)}
         <div
           className="h-full w-full absolute z-0 top-0 left-0"
           style={{
             backgroundImage: `url(${businessDetails?.logo})`,
             backgroundRepeat: "no-repeat",
-            backgroundSize:"cover",
+            backgroundSize: "cover",
             opacity: "0.2",
           }}
         ></div>
@@ -356,6 +358,12 @@ const BusinessDetails = () => {
                 if (item.tag === "twitter") {
                   return <Twitter url={item?.link} />;
                 }
+                if (item.tag === "youtube") {
+                  return <Youtube url={item?.link} />;
+                }
+                if (item.tag === "linkedIn") {
+                  return <LinkedIn url={item?.link} />;
+                }
               })}
 
               <button
@@ -439,6 +447,31 @@ const BusinessDetails = () => {
                   }
                   onChange={handleChange}
                   placeholder="Instagram"
+                  className="border border-gray-300 p-2 w-full rounded"
+                />
+
+                <input
+                  type="text"
+                  name="youtube"
+                  value={
+                    formData?.socialMediaLinks.find(
+                      (item) => item.tag === "youtube"
+                    )?.link
+                  }
+                  onChange={handleChange}
+                  placeholder="Youtube"
+                  className="border border-gray-300 p-2 w-full rounded"
+                />
+                <input
+                  type="text"
+                  name="linkedIn"
+                  value={
+                    formData?.socialMediaLinks.find(
+                      (item) => item.tag === "linkedIn"
+                    )?.link
+                  }
+                  onChange={handleChange}
+                  placeholder="linkedIn"
                   className="border border-gray-300 p-2 w-full rounded"
                 />
               </div>
@@ -757,6 +790,37 @@ const Instagram = ({ url }) => {
           d="M1.65396 4.27606C1 5.55953 1 7.23969 1 10.6V13.4C1 16.7603 1 18.4405 1.65396 19.7239C2.2292 20.8529 3.14708 21.7708 4.27606 22.346C5.55953 23 7.23969 23 10.6 23H13.4C16.7603 23 18.4405 23 19.7239 22.346C20.8529 21.7708 21.7708 20.8529 22.346 19.7239C23 18.4405 23 16.7603 23 13.4V10.6C23 7.23969 23 5.55953 22.346 4.27606C21.7708 3.14708 20.8529 2.2292 19.7239 1.65396C18.4405 1 16.7603 1 13.4 1H10.6C7.23969 1 5.55953 1 4.27606 1.65396C3.14708 2.2292 2.2292 3.14708 1.65396 4.27606ZM13.4 3H10.6C8.88684 3 7.72225 3.00156 6.82208 3.0751C5.94524 3.14674 5.49684 3.27659 5.18404 3.43597C4.43139 3.81947 3.81947 4.43139 3.43597 5.18404C3.27659 5.49684 3.14674 5.94524 3.0751 6.82208C3.00156 7.72225 3 8.88684 3 10.6V13.4C3 15.1132 3.00156 16.2777 3.0751 17.1779C3.14674 18.0548 3.27659 18.5032 3.43597 18.816C3.81947 19.5686 4.43139 20.1805 5.18404 20.564C5.49684 20.7234 5.94524 20.8533 6.82208 20.9249C7.72225 20.9984 8.88684 21 10.6 21H13.4C15.1132 21 16.2777 20.9984 17.1779 20.9249C18.0548 20.8533 18.5032 20.7234 18.816 20.564C19.5686 20.1805 20.1805 19.5686 20.564 18.816C20.7234 18.5032 20.8533 18.0548 20.9249 17.1779C20.9984 16.2777 21 15.1132 21 13.4V10.6C21 8.88684 20.9984 7.72225 20.9249 6.82208C20.8533 5.94524 20.7234 5.49684 20.564 5.18404C20.1805 4.43139 19.5686 3.81947 18.816 3.43597C18.5032 3.27659 18.0548 3.14674 17.1779 3.0751C16.2777 3.00156 15.1132 3 13.4 3Z"
           fill="#0F0F0F"
         />
+      </svg>
+    </a>
+  );
+};
+
+const LinkedIn = ({ url }) => {
+  return (
+    <a className="cursor-pointer" target="_blank" href={url}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 50 50"
+        width="50px"
+        height="50px"
+      >
+        {" "}
+        <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z" />
+      </svg>
+    </a>
+  );
+};
+
+const Youtube = ({ url }) => {
+  return (
+    <a className="cursor-pointer" target="_blank" href={url}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 50 50"
+        width="50px"
+        height="50px"
+      >
+        <path d="M 44.898438 14.5 C 44.5 12.300781 42.601563 10.699219 40.398438 10.199219 C 37.101563 9.5 31 9 24.398438 9 C 17.800781 9 11.601563 9.5 8.300781 10.199219 C 6.101563 10.699219 4.199219 12.199219 3.800781 14.5 C 3.398438 17 3 20.5 3 25 C 3 29.5 3.398438 33 3.898438 35.5 C 4.300781 37.699219 6.199219 39.300781 8.398438 39.800781 C 11.898438 40.5 17.898438 41 24.5 41 C 31.101563 41 37.101563 40.5 40.601563 39.800781 C 42.800781 39.300781 44.699219 37.800781 45.101563 35.5 C 45.5 33 46 29.398438 46.101563 25 C 45.898438 20.5 45.398438 17 44.898438 14.5 Z M 19 32 L 19 18 L 31.199219 25 Z" />
       </svg>
     </a>
   );
