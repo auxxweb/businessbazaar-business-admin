0010d04919527e45107162aa4617975a41a27ddd
Karthikk7293 <karthikk7293@gmail.com>
2024-12-03 17:56:58 +0530

    👌 IMPROVE: razorpay page

--- src/components/Razorpay/Razorpay.jsx
+++ src/components/Razorpay/Razorpay.jsx
@@ -1,39 +1,29 @@
 import { useEffect, useState } from "react";
-
 import { useLocation, useNavigate } from "react-router";
 import { toast } from "react-toastify";
-import { useDispatch, useSelector } from "react-redux";
-
-
 import useBusiness from "../../api/useBusiness";
 import Loader from "../Loader/Loader";
 import { checkPaymentStatus, createPayment } from "../../common/functions";
 
-
 export default function Razorpay() {
-  const dispatch = useDispatch();
-  
-  const [id,setId]=useState('')
+  const [id, setId] = useState('')
   const [isScriptLoaded, setScriptLoaded] = useState(false);
   const [loader, setLoader] = useState(false);
-  const [businessData,setBusinessData]=useState([])
+  const [businessData, setBusinessData] = useState([])
   const navigate = useNavigate()
-  const {  businesses,getBusiness } = useBusiness();
+  const { businesses, getBusiness } = useBusiness();
 
-  useEffect(()=>{
+  useEffect(() => {
     setBusinessData(businesses)
-  },[businesses])
-  // const planDetails = useSelector((state) => state.planDetails);
+  }, [businesses])
+
   const location = useLocation();
-
-  const  planDetails  = location.state || {}; // Destructure state
-
+  const planDetails = location.state || {}; // Destructure state
 
   useEffect(() => {
     const fetchData = async () => {
       getBusiness();
     };
-
     fetchData();
   }, []);
 
@@ -55,50 +45,50 @@ export default function Razorpay() {
 
   const handlePrevStep = () => navigate("/subscription");
 
-//     const options = {
-//       key: 'rzp_test_DBApSwEptkCDdS', // Dummy Razorpay key ID for testing
-//       amount: planDetails?.amount * 100, // Amount in paise (50000 paise = ₹500)
-//       currency: 'INR',
-//       name: 'InConnect',
-//       description:
-//         'InConnect is a comprehensive platform designed to simplify and enhance professional networking, providing seamless tools for business growth, collaboration, and community building',
-//       image: 'https://instant-connect.in/src/assets/images/enConnectLogo.jpeg', // Dummy logo URL
-//       handler: async function (response) {
-//         console.log(response, 'response')
-//         setLoader(true)
-//         const interval = setInterval(async () => {
-//           try {
-//             const paymentData = await checkPaymentStatus(id, token)
-//             const payment_status = paymentData?.data?.PaymentStatus
-//             if (payment_status === 'success') {
-//               setLoader(false)
-//               clearInterval(interval) // Clear the interval if payment is successful
-//               navigate(`/template/${id}`)
-//             }
-//             if (payment_status === 'failed') {
-//               setLoader(false)
-//               clearInterval(interval)
-//               toast.error('Payment Failed ,try again ', {
-//                 position: 'top-right',
-//                 autoClose: 3000,
-//                 hideProgressBar: false,
-//                 closeOnClick: true,
-//                 pauseOnHover: true,
-//                 draggable: true,
-//                 theme: 'colored',
-//                 style: {
-//                   backgroundColor: '#e74c3c', // Custom red color for error
-//                   color: '#FFFFFF', // White text
-//                 },
-//               })
-//               setStep((prev) => prev - 1)
-//             }
-//           } catch (error) {
-//             setLoader(false)
-//             setStep((prev) => prev - 1)
-//             console.error('Error fetching payment status:', error)
-//           }
-//         }, 2000)
+  //     const options = {
+  //       key: 'rzp_test_DBApSwEptkCDdS', // Dummy Razorpay key ID for testing
+  //       amount: planDetails?.amount * 100, // Amount in paise (50000 paise = ₹500)
+  //       currency: 'INR',
+  //       name: 'InConnect',
+  //       description:
+  //         'InConnect is a comprehensive platform designed to simplify and enhance professional networking, providing seamless tools for business growth, collaboration, and community building',
+  //       image: 'https://instant-connect.in/src/assets/images/enConnectLogo.jpeg', // Dummy logo URL
+  //       handler: async function (response) {
+  //         console.log(response, 'response')
+  //         setLoader(true)
+  //         const interval = setInterval(async () => {
+  //           try {
+  //             const paymentData = await checkPaymentStatus(id, token)
+  //             const payment_status = paymentData?.data?.PaymentStatus
+  //             if (payment_status === 'success') {
+  //               setLoader(false)
+  //               clearInterval(interval) // Clear the interval if payment is successful
+  //               navigate(`/template/${id}`)
+  //             }
+  //             if (payment_status === 'failed') {
+  //               setLoader(false)
+  //               clearInterval(interval)
+  //               toast.error('Payment Failed ,try again ', {
+  //                 position: 'top-right',
+  //                 autoClose: 3000,
+  //                 hideProgressBar: false,
+  //                 closeOnClick: true,
+  //                 pauseOnHover: true,
+  //                 draggable: true,
+  //                 theme: 'colored',
+  //                 style: {
+  //                   backgroundColor: '#e74c3c', // Custom red color for error
+  //                   color: '#FFFFFF', // White text
+  //                 },
+  //               })
+  //               setStep((prev) => prev - 1)
+  //             }
+  //           } catch (error) {
+  //             setLoader(false)
+  //             setStep((prev) => prev - 1)
+  //             console.error('Error fetching payment status:', error)
+  //           }
+  //         }, 2000)
   // Function to open Razorpay payment window
   const handlePayment = async (id, token) => {
     if (!isScriptLoaded) {
@@ -123,15 +113,15 @@ export default function Razorpay() {
         const interval = setInterval(async () => {
           try {
             const paymentData = await checkPaymentStatus(token);
-            console.log(paymentData,'payment dataaaaaaaa')
+            console.log(paymentData, 'payment dataaaaaaaa')
             const payment_status = paymentData?.data?.PaymentStatus;
-            const id= paymentData?.business
+            const id = paymentData?.business
             if (payment_status === "success") {
               setLoader(false);
               clearInterval(interval); // Clear the interval if payment is successful
               navigate(`/subscription`);
-            //   dispatch(resetBusinessState());
-            //   dispatch(resetPlanState());
+              //   dispatch(resetBusinessState());
+              //   dispatch(resetPlanState());
             }
             if (payment_status === "failed") {
               setLoader(false);
@@ -207,22 +197,20 @@ export default function Razorpay() {
 
   useEffect(() => {
     const submitData = async () => {
-        try { 
-         
-            const id = businessData._id
-            const token = JSON.parse(localStorage.getItem('userCredential'))
-            console.log(businessData,'kjkj')
-            console.log(planDetails.plan,'ithaaan ath')
-            const paymentRes= await createPayment({plan:planDetails.plan},token)
-            if(paymentRes.success){
-                handlePayment(id, token)
-            }    
-  
-        } catch (error) {
-          console.log(error, "razorpay-error");
-        }
-      
+      try {
 
+        const id = businessData._id
+        const token = JSON.parse(localStorage.getItem('userCredential'))
+        console.log(businessData, 'kjkj')
+        console.log(planDetails.plan, 'ithaaan ath')
+        const paymentRes = await createPayment({ plan: planDetails.plan }, token)
+        if (paymentRes.success) {
+          handlePayment(id, token)
+        }
+
+      } catch (error) {
+        console.log(error, "razorpay-error");
+      }
     };
 
     submitData();
